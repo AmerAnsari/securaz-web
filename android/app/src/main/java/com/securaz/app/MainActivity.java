e01e2f13e7d485df5723337b643f8a992cd446a9
Amer Ansari <amer@savandbros.com>
2024-08-22 15:31:28 +0400

    Add Android

---
+++ android/app/src/main/java/com/securaz/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.securaz.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.securaz.app',
+  appName: 'securaz',
+  webDir: 'dist/securaz-web/browser'
+};
+
+export default config;
